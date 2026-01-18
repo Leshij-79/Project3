626c11a7fc314477b5637aa188e9f449755d024a
Kuprin Aleksandr <freeman8901@yandex.ru>
2026-01-18 23:12:02 +0500

    Дополнительный результат

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
 
 def home(request):
     return render(request, 'home.html')
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return HttpResponse(f"{name}, благодарим за обращение! Ваше сообщение {message} получено. "
+                            f"С вами свяжутся по номеру {phone}")
+
     return render(request, 'contacts.html')
 
