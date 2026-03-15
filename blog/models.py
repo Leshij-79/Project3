bfc2c09de6a1129411fa3e448fbb7173f42ac5a0
Kuprin Aleksandr <freeman8901@yandex.ru>
2026-03-15 19:45:32 +0500

    25-2 миграции

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    heading = models.CharField(
+        max_length=150,
+        verbose_name="Заголовок",
+        unique=True,
+        help_text="Заголовок поста блога",
+    )
+    content = models.TextField(
+        blank=True,
+        help_text="Текст поста блога",
+    )
+    photo_blog = models.ImageField(
+        blank=True,
+        upload_to="photo/",
+        verbose_name="Фотография блога",
+        help_text="Фотография блога",
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name="Дата создания поста",
+        help_text="Дата создания поста",
+    )
+    ispublication = models.BooleanField(
+        verbose_name="Опубликовано",
+        help_text="Опубликовано",
+        default=False,
+    )
+    number_views = models.IntegerField(
+        default=0,
+        verbose_name="Количество просмотров",
+        help_text="Количество просмотров"
+    )
