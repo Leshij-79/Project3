936e5742454d280f03bb9905904b484995fb79d4
Kuprin Aleksandr <freeman8901@yandex.ru>
2026-03-15 20:03:54 +0500

    25-2 урлы

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from blog.apps import BlogConfig
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogDeleteView, BlogUpdateView
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path("blog/", BlogListView.as_view(), name="blog_list"),
+    path("blog/blog_create", BlogCreateView.as_view(), name="blog_create"),
+    path("blog/blog_detail/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("blog/blog_delete/<ink:pk>/", BlogDeleteView.as_view(), name="blog_delete"),
+    path("blog/blog_update/<ink:pk>", BlogUpdateView.as_view(), name="blog_update"),
+]
