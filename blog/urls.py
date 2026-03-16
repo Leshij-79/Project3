8c3f2ebb78a0ccc106e4d4ca8e7de79cea5cb55b
Kuprin Aleksandr <freeman8901@yandex.ru>
2026-03-16 21:43:12 +0500

    25-2 блог ссылки

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("blog_create/", BlogCreateView.as_view(), name="blog_create"),
     path("blog_detail/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
     # path("blog_delete/<ink:pk>/", BlogDeleteView.as_view(), name="blog_delete"),
-    # path("blog_update/<ink:pk>/", BlogUpdateView.as_view(), name="blog_update"),
+    path("blog_update/<int:pk>/", BlogUpdateView.as_view(), name="blog_update"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
 from blog.models import Blog
@@ -12,8 +12,15 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Blog
+    fields = ["heading", "content", "photo_blog", ]
     template_name = "blog_detail.html"
     success_url = reverse_lazy('blog:blog_list')
+
+    def get_object(self, queryset = None):
+        self.object = super().get_object(queryset)
+        self.object.number_views += 1
+        self.object.save()
+        return self.object
 
 
 class BlogCreateView(CreateView):
@@ -31,5 +38,9 @@ class BlogDeleteView(DeleteView):
 
 class BlogUpdateView(UpdateView):
     model = Blog
+    fields = ["heading", "content", "photo_blog",]
     template_name = "blog_update.html"
-    success_url = reverse_lazy('blog:blog_detail')
+    success_url = reverse_lazy('blog:blog_list')
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
