99d27a51ad049ead7bb083ee53f99f17f8edd19e
Kuprin Aleksandr <freeman8901@yandex.ru>
2026-03-22 16:34:42 +0500

    Задание 26.1 2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ContactsFormView, ProductDetailView, ProductListView
+from catalog.views import ContactsFormView, ProductDetailView, ProductListView, ProductUpdateView, ProductCreateView, \
+    ProductDeleteView
 
 app_name = CatalogConfig.name
 
@@ -9,5 +10,7 @@ urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
     path("contacts/", ContactsFormView.as_view(), name="contacts"),
     path("product_detail/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
-
+    path("product_update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
+    path("product_create/", ProductCreateView.as_view(), name="product_create"),
+    path("product_delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
 ]
